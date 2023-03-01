5106076fe15dee196e404cfeda96fbd51a6c61a0
diegosg99 <diegosg22360@gmail.com>
2023-03-01 09:11:40 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
