c6431c1f325938b0eb130c15cab716c04452ebba
diegosg99 <diegosg22360@gmail.com>
2023-03-07 15:23:46 +0100

    excel conversor almost working

--- src/app/shared/models/course.model.ts
+++ src/app/shared/models/course.model.ts
@@ -1,37 +1,78 @@
-export interface CourseDto {
-    code: string;
-    name: string;
-    tutor: string;
-    room:number;
-    day:string;
-    documentation: string;
+// export interface CourseDto {
+//     code: string;
+//     name: string;
+//     tutor: string;
+//     room:number;
+//     day:string;
+//     documentation: string;
 
-}
+// }
+
+type CloseState = 'No Finalizado' | '[0-15]' | '[15-30]' | 'Más de 30';
+type Confirmation = 'SI' | 'NO';
+
+export interface CourseDto {
+    code: String;
+    name: String;
+    start: string | number | null;
+    end: string | number | null;
+    preStart: string | number | null;
+    preEnd: string | number | null;
+    endDate: string | number | null;
+    place: string | null;
+    province: string | null;
+    solicitudes: number | string;
+    enrollments: number | string;
+    realized: number | null | string;
+    passed: number | null | string;
+    acreditation: Confirmation | string;
+    expedientNum: String | null;
+    creditNum: String | null;
+    daysToClose: number | null | string;
+    closeState: CloseState | string;
+  };
   
 export class Course {
-public code: string;
-public name: string;
-public tutor: string;
-public room: number;
-public day: string;
-public documentation: string;
+    private code: String;
+    private name: String;
+    private start: string | number | null;
+    private end: string | number | null;
+    private preStart: string | number | null;
+    private preEnd: string | number | null;
+    private endDate: string | number | null;
+    private place: string | null;
+    private province: string | null;
+    private solicitudes: number | string;
+    private enrollments: number | string;
+    private realized: number | null | string;
+    private passed: number | null | string;
+    private acreditation: Confirmation | string;
+    private expedientNum: String | null;
+    private creditNum: String | null;
+    private daysToClose: number | null | string;
+    private closeState: CloseState | string;
 
 
-    constructor({ code,name,tutor,room,day,documentation }: CourseDto = {
-        code:"",
-        name: "",
-        tutor: "",
-        room: 0,
-        day: "",
-        documentation: "",
-        }
+    constructor({ code,name,start,end,preStart,preEnd,endDate,place,province,solicitudes,enrollments,realized,passed,acreditation,expedientNum,creditNum,daysToClose,closeState }: CourseDto
     ) {
-        this.code = code;
-        this.name = name;
-        this.tutor = tutor;
-        this.room = room;
-        this.day = day;
-        this.documentation = documentation;
+       this.code = code;
+       this.name = name;
+       this.start = start;
+       this.end = end;
+       this.preStart = preStart;
+       this.preEnd = preEnd;
+       this.endDate = endDate;
+       this.place = place;
+       this.province = province;
+       this.solicitudes = solicitudes;
+       this.enrollments = enrollments;
+       this.realized = realized;
+       this.passed = passed;
+       this.acreditation = acreditation;
+       this.expedientNum = expedientNum;
+       this.creditNum = creditNum;
+       this.daysToClose = daysToClose;
+       this.closeState = closeState;
     }
 }
   
--- src/app/views/excel/excel.component.ts
+++ src/app/views/excel/excel.component.ts
@@ -1,32 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-//import * as path from 'path';
-//ng import * as Excel from 'exceljs';
+import { CourseDto,Course } from 'src/app/shared/models/course.model';
+import  * as Excel from 'exceljs'
 
 declare var window:any;
-//const filePath = path.resolve(__dirname, 'olympic-hockey-player.xlsx');
-
-// type Team = 'M' | 'W';
-// type Country = 'Canada' | 'USA';
-// type Position = 'Goalie' | 'Defence' | 'Forward';
-
-// type User = {
-//   id: number;
-//   team: Team;
-//   country: Country;
-//   firstName: string;
-//   lastName: string;
-//   weight: number;
-//   height: number;
-//   dateOfBirth: string; // (YYY-MM-DD)
-//   hometown: string;
-//   province: string;
-//   position: Position;
-//   age: number;
-//   heightFt: number;
-//   htln: number;
-//   bmi: number;
-  
-// };
 
 @Component({
   selector: 'app-excel',
@@ -34,64 +10,137 @@ declare var window:any;
   styleUrls: ['./excel.component.css']
 })
 
+//------------------------------------------- CLASS ------------------------------------------
+
 export class ExcelComponent implements OnInit {
 
   formModal:any;
   file:any;
+  e:Event | any;
+  data: any;
+
+  constructor(){
+  }
 
   ngOnInit(): void {
     this.formModal = new window.bootstrap.Modal(
       this.file = document.getElementById("importExcel")
     )
   }
+
+//------------------------------------------- DOM ------------------------------------------
+
   openModal() {
     this.formModal.show();
   }
   closeModal() {
     this.formModal.hide();
   }
-  importar = async () => {
-    // const workbook = new Excel.Workbook();
-    // const content = await workbook.xlsx.readFile(this.file);
-  
-    // const worksheet = content.worksheets[1];
-    // const rowStartIndex = 4;
-    // const numberOfRows = worksheet.rowCount - 3;
-  
-    // const rows = worksheet.getRows(rowStartIndex, numberOfRows) ?? [];
-  
-    // const players = rows.map((row): User => {
-    //   return {
-    //     // @ts-ignore
-    //     id: this.getCellValue(row,1),
-    //     // @ts-ignore
-    //     team: this.getCellValue(row, 2),
-    //     // @ts-ignore
-    //     country: this.getCellValue(row, 3),
-    //     firstName: this.getCellValue(row, 4),
-    //     lastName: this.getCellValue(row, 5),
-    //     // @ts-ignore
-    //     weight: this.getCellValue(row, 6),
-    //     height: +this.getCellValue(row, 7),
-    //     dateOfBirth: this.getCellValue(row, 8), // (YYY-MM-DD)
-    //     hometown: this.getCellValue(row, 9),
-    //     province: this.getCellValue(row, 10),
-    //     // @ts-ignore
-    //     position: this.getCellValue(row, 11),
-    //     age: +this.getCellValue(row, 12),
-    //     heightFt: +this.getCellValue(row, 13),
-    //     htln: +this.getCellValue(row, 14),
-    //     bmi: +this.getCellValue(row, 15),
-    //   }
-    // });
-  
-    // console.log(players);
-  };
-  // getCellValue = (row:  Excel.Row, cellIndex: number) => {
-  //   const cell = row.getCell(cellIndex);
-    
-  //   return cell.value ? cell.value.toString() : '';
-  // };
-}
 
-//importar().then();
+//------------------------------------------- EXCEL ------------------------------------------
+
+  loadFile(e:Event|any) {
+    this.file = e.target.files[0];
+  }
+
+  convert = async (docType:string) => {
+
+    let reader = new FileReader();
+    reader.readAsArrayBuffer(this.file);
+    reader.onload = () => {
+    const buffer:any = reader.result;
+    const wb = new Excel.Workbook();
+
+//------------------------------------------- Crear array de rows ------------------------------------------
+
+    let rows:any|CourseDto = [];      
+
+    wb.xlsx.load(buffer).then(workbook => {
+
+    let sheet = workbook.worksheets[0];
+      sheet.eachRow((row) => {
+        rows.push(row.values);
+      })
+    })
+    
+    //-------------------------- INTENTO DE QUITAR LOS FILAS DE ARRIBA -----------------------------
+    // for (let i = 0; i < invalidRows; i++) {
+    //   let el = rows.shift();
+    // }
+    //let newData:any = []; ----------------TODO
+
+    if (docType =='cursos') {
+      
+      console.log(rows);
+
+      this.data = rows.forEach((row:Excel.Row): Course | undefined => {
+        console.log('ola');
+        try {
+          let course =  new Course({
+            code: this.getCellValue(row,1),
+            name: this.getCellValue(row, 2),
+            start: this.getCellValue(row, 3),
+            end: this.getCellValue(row, 4),
+            preStart: this.getCellValue(row, 5),
+            preEnd: this.getCellValue(row, 6),
+            endDate: this.getCellValue(row, 7),
+            place: this.getCellValue(row, 8),
+            province: this.getCellValue(row, 9),
+            solicitudes: this.getCellValue(row, 10),
+            enrollments: this.getCellValue(row, 11),
+            realized: this.getCellValue(row, 12),
+            passed: this.getCellValue(row, 13),
+            acreditation: this.getCellValue(row, 14),
+            expedientNum: this.getCellValue(row, 15),
+            creditNum: this.getCellValue(row, 16),
+            daysToClose: this.getCellValue(row,17),
+            closeState: this.getCellValue(row,18)
+          });
+          
+          console.log(course);
+  
+          //newData.push(course);
+  
+          return new Course({
+            code: this.getCellValue(row,1),
+            name: this.getCellValue(row, 2),
+            start: this.getCellValue(row, 3),
+            end: this.getCellValue(row, 4),
+            preStart: this.getCellValue(row, 5),
+            preEnd: this.getCellValue(row, 6),
+            endDate: this.getCellValue(row, 7),
+            place: this.getCellValue(row, 8),
+            province: this.getCellValue(row, 9),
+            solicitudes: this.getCellValue(row, 10),
+            enrollments: this.getCellValue(row, 11),
+            realized: this.getCellValue(row, 12),
+            passed: this.getCellValue(row, 13),
+            acreditation: this.getCellValue(row, 14),
+            expedientNum: this.getCellValue(row, 15),
+            creditNum: this.getCellValue(row, 16),
+            daysToClose: this.getCellValue(row,17),
+            closeState: this.getCellValue(row,18)
+          })
+        } catch (error) {
+          console.log(error);
+          return
+        }
+        
+      });
+    }
+
+    if (docType==='docentes') {
+    }
+    if (docType==='alumnos') {
+    }
+    return this.data;
+    };
+  }
+  getCellValue = (row:  Excel.Row, cellIndex: number) => {
+    const cell = row.getCell(cellIndex);
+    
+    return cell.value ? cell.value.toString() : '';
+  };
+  importar(){}
+
+}
