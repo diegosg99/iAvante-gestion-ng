9ef2c975523b644e6758f30e586e17c7e128514d
diegosg99 <diegosg22360@gmail.com>
2023-03-16 16:44:38 +0100

    integrating security and modifying register and login

--- apiRest.js
+++ apiRest.js
@@ -3,15 +3,26 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 const mysql = require('mysql');
 const moment = require('moment');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 
 const formatComplete = 'YYYY-MM-DD HH:mm:ss'
+
+const SECRET = "614f4f4a6568e9ae881c76e8753f65c9";
 
 const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'asistencia'
+});
+
+const adminDB = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'iavante'
 });
 
 connection.connect();
@@ -126,11 +137,6 @@ app.post('/courses/uploadExcel',(req,res) => {
   try{
     let data = req.body;
 
-//IF NOT EXISTS (select code from cursos where code='${item.code}')
-//...
-//BEGIN
-//...
-
     data.forEach(item =>{
       let sql = `INSERT INTO cursos VALUES ('${item.id}','${item.code}','${item.name}','${item.tutor}','${item.room}','${item.day}','${item.documentation}')`;
 
@@ -144,6 +150,80 @@ app.post('/courses/uploadExcel',(req,res) => {
   }
 })
 
+//---------------------->  Control de administradores  <-------------------------
+
+app.post('/admins/register',async (req,res) => {
+  try{
+    let item = req.body;
+    let encodedPassword = await encodePassword(item.password);
+    console.log(encodedPassword);
+
+    let sql = `INSERT INTO admins VALUES ('${item.id}','${item.name}','${item.dni}','${item.username}','${item.email}','${item.phone}','${encodedPassword}','${item.photo}')`;
+
+    adminDB.query(sql, function(err, rows, fields) {
+        if (err) throw err;
+        res.status(200).send("exito");
+    });
+
+  }catch(error) {
+    res.status(400).send(item,req);
+  }
+})
+
+app.post('/admins/login', async (req,res) => {
+  try{
+    let data = req.body;
+    
+    console.log(data);
+
+    const token = jwt.sign(data.username,SECRET);
+    data.token = token;
+
+    let sql = `SELECT username,password FROM admins WHERE username = '${data.username}'`;
+
+    adminDB.query(sql, function(err, rows, fields) {
+
+    bcrypt.compare(data.password,rows[0].password).then(bool=>{
+
+      if (bool===true) {
+        res.status(200).send(data);
+      }
+      
+      else{
+        res.status(300).send("Credenciales incorrectas");
+      }   
+    });   
+  })
+  }catch(error) {
+    res.status(400).send(req);
+  }
+})
+
+
+app.get('/admins/checkToken/:token',(req,res) => {
+  try{
+    let token = req.params.token;
+    let username = jwt.verify(token,SECRET);
+  
+    res.status(200).send(JSON.stringify(username))
+  
+  }catch(error) {
+    res.status(400).send(req);
+  }
+})
+
+//--------------------Encode/Decode Passwords---------------------------
+
+encodePassword = async (password) => {
+  const hash = await bcrypt.hash(password, 10);
+  return hash;
+}
+
+comparePassword = async (plaintextPassword, hash) => {
+  const result = await bcrypt.compare(plaintextPassword, hash);
+  return result;
+}
+//----------------------------------------------------
 app.listen(3003, () =>
   console.log(`¡Aplicación escuchando en el puerto 3003!`),
 );
